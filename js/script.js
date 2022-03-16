281171c158dd70f18fc29efee6993880148ac3bc
Cole <cole.smith.code@gmail.com>
2022-03-16 09:59:37 -0600

    added a search bar, and necc varibles

--- js/script.js
+++ js/script.js
@@ -80,3 +80,17 @@ function addPagination (list) {
 // Call functions
 showPage();
 addPagination();
+
+
+const searchBar = `
+   <label for="search" class="student-search">
+      <span>Search by name</span>
+      <input id="search" placeholder="Search by name...">
+      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+   </label>`;
+document.querySelector('.header').insertAdjacentHTML('beforeend', searchBar);
+
+
+const search = document.querySelector('#search');
+const submit = document.querySelector('.student-search button');
+let studentMatches = [];
